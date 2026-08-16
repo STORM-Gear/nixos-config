48290ea56f5059669ce337e161dafbd6ae1731e1
GaspardCulis <gaspard@gasdev.fr>
2026-08-16 14:32:03 +0200

    feat: Setup devshell

--- flake.nix
+++ flake.nix
@@ -11,12 +11,26 @@
     self,
     nixpkgs,
     ...
-  } @ inputs: {
+  } @ inputs: let
+    systems = ["x86_64-linux" "aarch64-linux"];
+    forAllSystems = f: nixpkgs.lib.genAttrs systems f;
+    nixpkgsFor = system: import nixpkgs {inherit system;};
+  in {
     nixosConfigurations.storm = nixpkgs.lib.nixosSystem {
       modules = [
         inputs.storm-backend.nixosModules.default
         ./configuration.nix
       ];
     };
+
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgsFor system;
+    in {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          sops
+        ];
+      };
+    });
   };
 }
